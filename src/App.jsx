e84a48e2c56ead25ee5e10ee6cc7aa0daac1bd4f
Abishek Padaki <abiks@Abisheks-MacBook-Air.local>
2023-03-16 13:17:14 -0700

    changes to main app and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,31 @@
 import { useState } from 'react'
 import { Configuration, OpenAIApi } from 'openai'
 import './App.css'
-const API_KEY = import.meta.env.VITE_Open_AI_Key 
+// const API_KEY = import.meta.env.VITE_Open_AI_Key 
 
 function App() {
+  const [prompt, setPrompt] = useState('')
   const configuration = new Configuration({
-    apiKey: API_KEY,
+    apiKey: '',
   });
-  const openai = new OpenAIApi(configuration);
+  // const openai = new OpenAIApi(configuration);
+
   const generateImage = async() => {
-    const res = await openai.createImage({
-      prompt: "Say this is a test",
-      n: 1,
-      size: '1024x1024',
-    });
-    console.log(res.data.data[0].url)
+    // const res = await openai.createImage({
+    //   prompt: prompt,
+    //   n: 1,
+    //   size: '1024x1024',
+    // });
+    // console.log(res.data.data[0].url)
+    console.log('ip')
   }
   
   return (
       <div className='app-main'>
-        <Input>
+        <h3>What would you like AbIks to generate?</h3>
+        <input className='app-input' onChange={(e) => setPrompt(e.target.value)}>
         <button onClick={generateImage()}>Generate Image</button>
-        </Input>   
+        </input>   
     </div>
   )
 }
